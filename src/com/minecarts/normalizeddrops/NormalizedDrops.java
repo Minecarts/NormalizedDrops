2dcb17814afd792327b6df9559878a72e1d67ff0
Kevin Ingersoll <kingersoll@gmail.com>
2011-10-28 22:35:19 -0700

    More spacing changes to make verrier mad :)

--- src/com/minecarts/normalizeddrops/NormalizedDrops.java
+++ src/com/minecarts/normalizeddrops/NormalizedDrops.java
@@ -11,7 +11,7 @@ import org.bukkit.util.config.Configuration;
 import com.minecarts.normalizeddrops.command.MainCommand;
 import com.minecarts.normalizeddrops.listener.*;
 
-public class NormalizedDrops extends org.bukkit.plugin.java.JavaPlugin{
+public class NormalizedDrops extends org.bukkit.plugin.java.JavaPlugin {
     
     public final Logger log = Logger.getLogger("com.minecarts.normalizeddrops");
     public EntityListener entityListener;
--- src/com/minecarts/normalizeddrops/command/MainCommand.java
+++ src/com/minecarts/normalizeddrops/command/MainCommand.java
@@ -6,17 +6,17 @@ import org.bukkit.command.ConsoleCommandSender;
 
 import com.minecarts.normalizeddrops.*;
 
-public class MainCommand extends CommandHandler{
+public class MainCommand extends CommandHandler {
     
-    public MainCommand(NormalizedDrops plugin){
+    public MainCommand(NormalizedDrops plugin) {
         super(plugin);
     }
     
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if(args.length == 1 && args[0].equalsIgnoreCase("reload")){
+        if(args.length == 1 && args[0].equalsIgnoreCase("reload")) {
             //Reload the config
-            if(sender.isOp() || sender instanceof ConsoleCommandSender){
+            if(sender.isOp() || sender instanceof ConsoleCommandSender) {
                 plugin.entityListener.setConfig(plugin.getConfiguration());
                 sender.sendMessage(plugin.pdf.getName() + " configuration reloaded.");
             }
